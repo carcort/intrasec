9441c4daad93720a71c1f2630de36ca4ddd4c0d0
Eric Carvajal Cortes <eric@carveri.carveri.com>
2026-07-12 15:04:30 -0400

    V14 Version 1 en GH

--- src/Public/Home/Home.tsx
+++ src/Public/Home/Home.tsx
@@ -27,18 +27,18 @@ const Home = () => {
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam consequatur, cum aperiam exercitationem aliquid libero? Ea mollitia cum aperiam quia at maxime minus voluptates! Voluptatum itaque enim quasi molestias reiciendis.
               </h2>
             </div>
-            <div className="w-full h-[25%] flex gap-x-5 ">
-              <button onClick={handleClickIngresar} className="bg-blue-600 text-white w-full h-[50%] cursor-pointer hover:bg-blue-700">
+            <div className="w-[90%] h-[25%] flex gap-x-5 font-semibold">
+              <button onClick={handleClickIngresar} className="bg-blue-600 text-white w-full h-[40%] cursor-pointer hover:bg-blue-700">
                 Ingresa
               </button>
-              <button className="bg-[#38ff02] text-white w-full h-[50%] cursor-pointer hover:bg-green-600"> 
+              <button className="bg-[#38ff02] text-white w-full h-[40%] cursor-pointer hover:bg-green-600"> 
                 Prueba Gratis!
               </button>
             </div>
            </article>
 
            <article className="w-full h-full grid place-items-center">
-              <div className="w-[70%] h-[70%]">
+              <div className="w-[70%] h-[60%]">
                   <img className="w-full h-full" src="https://roudev-s3-assets.s3.us-east-1.amazonaws.com/AssetsRoudev/Icons/logoPrincipal.png" alt="sd" />
               </div>
            </article>
